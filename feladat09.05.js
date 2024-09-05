3ce2ca98d1a25dca5e2d9c7b222a02a1ce14a87b
Ötvös Ákos <akika02531@gmail.com>
2024-09-05 10:47:22 +0200

    started

---
+++ feladat09.05.js
@@ -0,0 +1,8 @@
+function changeMonth() {
+    let selectedMonth = document.getElementById("honapok").value
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+}
+
